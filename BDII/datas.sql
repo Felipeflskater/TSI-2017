2b781fd9cd03f586b2ad8d335308ed464bbea0af
DESKTOP-PU8N4LO\FelipeFlskater <felipe.aranalde@gmail.com>
2018-08-13 23:40:19 -0300

    novos exercicios

---
+++ BDII/datas.sql
@@ -0,0 +1,24 @@
+﻿select length(nome) from clientes where nome ilike '%%'
+select * from clientes
+select 'Insert into clientes (codcliente, nome, endereco, tipo_cliente, rg, cpf, cnpj, obs) values
+('||codcliente||', '''||nome||''', '''||endereco||''', '''||tipo_cliente||''');' 
+ from clientes
+
+select concat (extract(month from current_date),' / ',extract(day from current_date))
+select date_part('month',current_date)
+select date_part('day',current_date)
+select age (timestamp '28/02/1995')
+
+select 'nome:' as descricao,'felipe prestes aranalde' as eu 
+union all
+select 'idade:' as descricao,concat (extract (year from (select age (timestamp '28/02/1995')))) as eu
+union all
+select 'Mes:' as descricao ,concat (extract (month from (select age (timestamp '28/02/1995')))) as eu
+union all
+select 'Dia:' as descricao ,concat (extract (day from (select age (timestamp '28/02/1995')))) as eu
+union all
+select 'to char data:' as descricao,to_char (current_date,'DD,MM,YYYY') as eu
+union all
+select 'Data monstra:' as descricao,to_char (current_date + 99999999   ,'DD,MM,YYYY') as eu
+union all
+select 'Data monstra:' as descricao,to_char (current_date + 99999999   ,'TMday "de" TMmont "de" TMyyyy') as eu
---
+++ BDII/lista4.sql
@@ -0,0 +1,19 @@
+﻿--Ex 1
+select distinct age(dt_venda) from vendas where codfunc in (select codfunc from funcionarios where nome ilike '%Mariana Costa%') 
+and dt_venda in (select max(dt_venda) from vendas where 
+codfunc in (select codfunc from funcionarios where nome like '%Mariana Costa%'))
+--EX 2
+select  valor * 1.5,codigo from itens where codigo in (2,3)
+--EX 3
+select current_date
+--EX 4
+select to_char (current_date, 'DD/MM/YY')
+--EX 5
+select distinct to_char(dt_venda,'TMmonth') from vendas where extract (year from dt_venda)=2017 and codfunc in (select codfunc from funcionarios where nome like '%Cristian Tavares%')
+--EX 6
+
+
+
+select ':D--|-<'
+
+
